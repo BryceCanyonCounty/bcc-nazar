e1c461ae9244d7fbe464f9ed4130982ba3355141
Jake2k4 <119366047+jakeyboi1@users.noreply.github.com>
2023-04-30 18:24:36 -0500

    Add files via upload

--- client/menusetup.lua
+++ client/menusetup.lua
@@ -7,9 +7,24 @@ TriggerEvent("menuapi:getData", function(call)
     MenuData = call
 end)
 
+--Input var setup
+local myInput = {
+    type = "enableinput", -- dont touch
+    inputType = "input", -- or text area for sending messages
+    button = Config.Language.input_button, -- button name
+    placeholder = Config.Language.input_placeholder, --placeholdername
+    style = "block", --- dont touch
+    attributes = {
+        inputHeader = Config.Language.input_header, -- header 1
+        type = "number", -- inputype text, number,date.etc if number comment out the pattern
+        pattern = "[0-9]{1,20}", -- regular expression validated for only numbers "[0-9]", for letters only [A-Za-z]+   with charecter limit  [A-Za-z]{5,20}     with chareceter limit and numbers [A-Za-z0-9]{5,}
+        title = "must be only numbers min 1 max 20", -- if input doesnt match show this message
+        style = "border-radius: 10px; background-color: ; border:none;", -- style  the inptup
+    }
+}
+
 --this is used to close the menu while you are on the main menu and hit backspace button
 local inmenu = false --var used to see if you are in the main menu or not
-RegisterNetEvent('bcc-nazar:MenuClose')
 AddEventHandler('bcc-nazar:MenuClose', function()
     while true do --loops will run permantely
         Citizen.Wait(10) --waits 10ms prevents crashing
@@ -82,25 +97,10 @@ function BuyMenu()
                 Iitemname = data.current.info.itemdbname --sets the varible to whatever option is clicked (since we set info to = the table)
                 Pprice = data.current.info.price
                 Scurrencytype = data.current.info.currencytype -- Get the currencytype from config.shop added by mrtb
-                local myInput = {
-                    type = "enableinput", -- dont touch
-                    inputType = "input", -- or text area for sending messages
-                    button = Config.Language.input_button, -- button name
-                    placeholder = Config.Language.input_placeholder, --placeholdername
-                    style = "block", --- dont touch
-                    attributes = {
-                        inputHeader = Config.Language.input_header, -- header 1
-                        type = "number", -- inputype text, number,date.etc if number comment out the pattern
-                        pattern = "[0-9]{1,20}", -- regular expression validated for only numbers "[0-9]", for letters only [A-Za-z]+   with charecter limit  [A-Za-z]{5,20}     with chareceter limit and numbers [A-Za-z0-9]{5,}
-                        title = "must be only numbers min 1 max 20", -- if input doesnt match show this message
-                        style = "border-radius: 10px; background-color: ; border:none;", -- style  the inptup
-                    }
-                }
                 TriggerEvent("vorpinputs:advancedInput", json.encode(myInput),function(result)
                     local qty = tonumber(result)
-                    if qty == nil then
-                    elseif qty > 0 then
-                        TriggerServerEvent("bcc-nazar:nazarsellinfopass",  qty) --result
+                    if qty > 0 then
+                        TriggerServerEvent('bcc-nazar:buyfromnazar', qty, Iitemname, Pprice, Scurrencytype)
                     else
                         TriggerEvent("vorp:TipRight", "insertamount", 3000)
                     end
@@ -138,25 +138,10 @@ function SellMenu()
                 Iitemname = data.current.info.itemdbname --sets the varible to whatever option is clicked
                 Pprice = data.current.info.price
                 Scurrencytype = data.current.info.currencytype --added by mrtb
-                local myInput = {
-                    type = "enableinput", -- dont touch
-                    inputType = "input", -- or text area for sending messages
-                    button = Config.Language.input_button, -- button name
-                    placeholder = Config.Language.input_placeholder, --placeholdername
-                    style = "block", --- dont touch
-                    attributes = {
-                        inputHeader = Config.Language.input_header, -- header 1
-                        type = "number", -- inputype text, number,date.etc if number comment out the pattern
-                        pattern = "[0-9]{1,20}", -- regular expression validated for only numbers "[0-9]", for letters only [A-Za-z]+   with charecter limit  [A-Za-z]{5,20}     with chareceter limit and numbers [A-Za-z0-9]{5,}
-                        title = "must be only numbers min 1 max 20", -- if input doesnt match show this message
-                        style = "border-radius: 10px; background-color: ; border:none;", -- style  the inptup
-                    }
-                }
                 TriggerEvent("vorpinputs:advancedInput", json.encode(myInput),function(result)
                     local qty = tonumber(result)
-                    if qty == nil then
-                    elseif qty > 0 then
-                        TriggerServerEvent("bcc-nazar:catchinputforsell",  qty) --result
+                    if qty > 0 then
+                        TriggerServerEvent('bcc-nazar:getplayerdataforsell', Iitemname, Pprice, qty, Scurrencytype)
                     else
                         TriggerEvent("vorp:TipRight", "insertamount", 3000)
                     end
@@ -193,11 +178,6 @@ function HintMenu()
                 C = data.current.info.location
                 V = data.current.info
                 TriggerServerEvent('bcc-nazar:menuopen6', cost) --triggers server event which is the cooldown event 
-                RegisterNetEvent('bcc-nazar:menuopen4') --creates a net event for the serever to call
-                AddEventHandler('bcc-nazar:menuopen4', function() --makes the net event have something to run
-                    VORPcore.NotifyBottomRight(Config.Language.HintNotify,6000) --text in bottom right
-                    searchforchest()
-                end)
             end
         end)
 end
@@ -208,17 +188,4 @@ AddEventHandler("onResourceStop", function(resourceName)
         return
     end
     MenuData.CloseAll()
-end)
-
---This recieves the qty when buying from nazar and then passes the qty along with the item name and price to the server to handle giving the items
-RegisterNetEvent('bcc-nazar:nazarsellableitemscatch')
-AddEventHandler('bcc-nazar:nazarsellableitemscatch', function(qty) --is catching the qty from the server
-    TriggerServerEvent('bcc-nazar:buyfromnazar', qty, Iitemname, Pprice, Scurrencytype) --is passing the 4 variables to the server Scurrencytype --edited by mrtb
-end)
-
-
---this recieves the qty when selling items to nazar from the server and then passes the itemname and price to the server along with the qty
-RegisterNetEvent('bcc-nazar:infosenderforsell')
-AddEventHandler('bcc-nazar:infosenderforsell', function(qty)
-    TriggerServerEvent('bcc-nazar:getplayerdataforsell', Iitemname, Pprice, qty, Scurrencytype) -- edited by mrtb
 end)
