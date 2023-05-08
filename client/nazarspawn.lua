72cf0939e39dd39805b6c39aa76005dd74711161
Mr Terabyte <gcpamitha@gmail.com>
2023-05-09 00:16:23 +0530

    - Small modifications (#14)
    - Added code modification to remove Ped, Blip and Wagon when resource stops

--- client/nazarspawn.lua
+++ client/nazarspawn.lua
@@ -4,6 +4,7 @@ TriggerEvent("getUtils", function(utils)
     VORPutils = utils
 end)
 BccUtils = exports['bcc-utils'].initiate()
+local blip, createdped, object
 
 
 --Thread to set nazars spawn location (this has to be done server side otherwise it would be different for each client)
@@ -18,7 +19,7 @@ RegisterNetEvent('bcc-nazar:pedspawn', function(nspawn)
 
     --Spawning Wagon Setup
     if Config.NazarSetup.nazarswagon then
-        local object = CreateObject('mp005_p_collectorwagon01', wagonspawn.x, wagonspawn.y, wagonspawn.z - 1, false, false, false) --creates an object
+        object = CreateObject('mp005_p_collectorwagon01', wagonspawn.x, wagonspawn.y, wagonspawn.z - 1, false, false, false) --creates an object
         RequestModel(object, true) --requests the object model
         PlaceObjectOnGroundProperly(object, true) --sets the wagon on ground properly
     end
@@ -28,7 +29,7 @@ RegisterNetEvent('bcc-nazar:pedspawn', function(nspawn)
     
     if Config.NazarSetup.blip then
         --Pulled from mrterabytes oil fork
-        local blip = Citizen.InvokeNative(0x554D9D53F696D002, 1664425300, Nspawn.x, Nspawn.y, Nspawn.z) -- This create a blip with a defualt blip hash we given
+        blip = Citizen.InvokeNative(0x554D9D53F696D002, 1664425300, Nspawn.x, Nspawn.y, Nspawn.z) -- This create a blip with a defualt blip hash we given
         SetBlipSprite(blip, Config.NazarSetup.BlipHash, 1) -- This sets the blip hash to the given in config.
         SetBlipScale(blip, 0.8)
         Citizen.InvokeNative(0x662D364ABF16DE2F, blip, joaat(Config.NazarSetup.BlipColor))
@@ -42,7 +43,7 @@ RegisterNetEvent('bcc-nazar:pedspawn', function(nspawn)
     while not HasModelLoaded(model) do
         Wait(100)
     end
-    local createdped = CreatePed(model, Nspawn.x, Nspawn.y, Nspawn.z - 1, Nspawn.h, false, true, true, true) --creates ped the minus one makes it so its standing on the ground not floating
+    createdped = CreatePed(model, Nspawn.x, Nspawn.y, Nspawn.z - 1, Nspawn.h, false, true, true, true) --creates ped the minus one makes it so its standing on the ground not floating
     Citizen.InvokeNative(0x283978A15512B2FE, createdped, true) -- sets ped into random outfit, stops it being invis
     BccUtils.Ped.SetStatic(createdped)
     TaskStartScenarioInPlace(createdped, joaat("WORLD_HUMAN_SMOKE_NAZAR"), -1)
@@ -73,5 +74,14 @@ RegisterNetEvent('bcc-nazar:pedspawn', function(nspawn)
             end
         end
         if dist > 200 then Citizen.Wait(2000) end
+    end
+end)
+
+-- Delete Blips, Peds and wagon when resource stops
+AddEventHandler("onResourceStop", function(resource)
+    if resource == GetCurrentResourceName() then
+        DeletePed(createdped)
+        RemoveBlip(blip)
+        DeleteEntity(object)
     end
 end)
