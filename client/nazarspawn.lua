1faefbb744f20d88ca21f7f7602626338742be5f
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-12-22 15:22:08 -0800

    Update nazarspawn.lua
    - update blip color format
    - add sleep to while loop
    - housekeeping

--- client/nazarspawn.lua
+++ client/nazarspawn.lua
@@ -1,8 +1,4 @@
 ------------------- Pulling Essentials ----------
-VORPutils = {}
-TriggerEvent("getUtils", function(utils)
-    VORPutils = utils
-end)
 BccUtils = exports['bcc-utils'].initiate()
 
 -------------- Variables -------------------
@@ -10,18 +6,18 @@ local blip, createdped, object
 
 ------------- Location Setting ---------------
 CreateThread(function()
-    TriggerServerEvent('bcc-nazar:locationset')
+    TriggerServerEvent('bcc-nazar:LocationSet')
 end)
 
 --------------- Nazar Spawn Setup -------------------
-RegisterNetEvent('bcc-nazar:pedspawn', function(nspawn)
+RegisterNetEvent('bcc-nazar:PedSpawn', function(nspawn)
     local audioplay, wagonspawn = false, nspawn.nazarwagonspawncoords
     Nspawn = nspawn.nazarspawncoords
 
     if Config.NazarSetup.nazarswagon then
         object = CreateObject('mp005_p_collectorwagon01', wagonspawn.x, wagonspawn.y, wagonspawn.z - 1, false, false, false)
         RequestModel(object, true)
-        PlaceObjectOnGroundProperly(object, true)
+        Citizen.InvokeNative(0x58A850EAEE20FAA3, object, true) -- PlaceObjectOnGroundProperly
     end
 
     if Config.NazarSetup.blip then
@@ -29,17 +25,14 @@ RegisterNetEvent('bcc-nazar:pedspawn', function(nspawn)
         blip = Citizen.InvokeNative(0x554D9D53F696D002, 1664425300, Nspawn.x, Nspawn.y, Nspawn.z) -- This create a blip with a defualt blip hash we given
         SetBlipSprite(blip, Config.NazarSetup.BlipHash, 1) -- This sets the blip hash to the given in config.
         SetBlipScale(blip, 0.8)
-        Citizen.InvokeNative(0x662D364ABF16DE2F, blip, joaat(Config.NazarSetup.BlipColor))
-        Citizen.InvokeNative(0x9CB1A1623062F402, blip, Config.NazarSetup.BlipName) -- Sets the blip Name
+        Citizen.InvokeNative(0x662D364ABF16DE2F, blip, joaat(Config.BlipColors[Config.NazarSetup.BlipColor])) -- BlipAddModifier
+        Citizen.InvokeNative(0x9CB1A1623062F402, blip, Config.NazarSetup.BlipName) -- SetBlipName
     end
-    
-    local model = joaat('cs_mp_travellingsaleswoman')
+
+    local model = joaat(Config.NazarSetup.Model)
     RequestModel(model)
-    if not HasModelLoaded(model) then
-        RequestModel(model)
-    end
     while not HasModelLoaded(model) do
-        Wait(100)
+        Wait(10)
     end
     createdped = CreatePed(model, Nspawn.x, Nspawn.y, Nspawn.z - 1, Nspawn.h, false, true, true, true) --creates ped the minus one makes it so its standing on the ground not floating
     Citizen.InvokeNative(0x283978A15512B2FE, createdped, true) -- sets ped into random outfit, stops it being invis
@@ -47,17 +40,17 @@ RegisterNetEvent('bcc-nazar:pedspawn', function(nspawn)
     TaskStartScenarioInPlace(createdped, joaat("WORLD_HUMAN_SMOKE_NAZAR"), -1)
 
     while true do
-        Wait(5)
-        local playercoord = GetEntityCoords(PlayerPedId())
-        local dist = GetDistanceBetweenCoords(playercoord.x, playercoord.y, playercoord.z, Nspawn.x, Nspawn.y, Nspawn.z, true)
-        if dist < 10 then
+        Wait(0)
+        local sleep = true
+        local distance = #(GetEntityCoords(PlayerPedId()) - vector3(Nspawn.x, Nspawn.y, Nspawn.z))
+        if distance <= 10 then
+            sleep = false
             if Config.NazarSetup.Music then
                 if not audioplay then
                     audioplay = true
                     BccUtils.YtAudioPlayer.PlayAudio('https://www.youtube.com/embed/IgSBKjBz7Qw', 'IgSBKjBz7Qw', Config.NazarSetup.MusicVolume, 1)
                 end
             end
-            BccUtils.Misc.DrawText3D(Nspawn.x, Nspawn.y, Nspawn.z, Config.Language.TalkToNPCText)
         else
             if Config.NazarSetup.Music then
                 if audioplay then
@@ -66,12 +59,16 @@ RegisterNetEvent('bcc-nazar:pedspawn', function(nspawn)
                 end
             end
         end
-        if dist < 2 then
-            if IsControlJustReleased(0, 0x760A9C6F) then
+        if distance <= 2 then
+            BccUtils.Misc.DrawText3D(Nspawn.x, Nspawn.y, Nspawn.z, _U('TalkToNPCText'))
+            if IsControlJustReleased(0, Config.keys.nazar) then -- [G]
                 MainMenu() --opens the menu
+                DisplayRadar(false)
             end
         end
-        if dist > 200 then Citizen.Wait(2000) end
+        if sleep then
+            Wait(1000)
+        end
     end
 end)
 
