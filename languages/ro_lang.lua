81b73d8e0ccf9c2202611ff06949972594c4244c
iseeyoucopy <iseeyoucopy@yahoo.com>
2024-02-25 22:38:48 +0000

    create ro_lang.lua
    Added Romanian language support

---
+++ languages/ro_lang.lua
@@ -0,0 +1,28 @@
+---------------------------------- Romana -------------------------------------------
+-- Va rugam sa trimiteti Pull Request pe GitHub cu Actualizarile de Limba
+Locales["ro_lang"] = {
+
+    TreasureBlipName = 'Comoara?',
+    TreasurePromptTitle = 'Cauta in lada dupa indicii',
+    TreasurePrompt_search = "Deschide",
+    TalkToNPCText = 'Apasa "G" pentru a vedea ce ofera Doamna Nazar',
+    Menu_SubTitle_Hint = 'Cumpara Indicii',
+    Menu_SubTitle_Sell = 'Vinde Obiecte',
+    Menu_SubTitle_Buy = 'Cumpara Obiecte',
+    Nazar = 'Doamna Nazar',
+    input_button = "confirma",
+    input_placeholder = "introduceti cantitatea",
+    input_header = "cantitate",
+    HintNotify = "Un indiciu catre o comoara a fost marcat",
+    NoHintNotify = "in prezent nu am nimic de oferit, reveniti mai tarziu",
+    NoItem = "Nu ai acest obiect.",
+    NoMoney = "Nu ai destui bani.",
+    NoGold = "Nu ai destul aur", -- adaugat de mrtb
+    ChestLooted = 'Obiecte primite: ',
+    Pickup = "Colecteaza",
+    YouCollected = "Ai colectat o carte",
+    CardCollected = "Carte colectata",
+    CantCollectCard = "Te rog asteapta, cineva a colectat recent aceasta carte",
+    StackFull = "Nu poti transporta mai multe din acest obiect",
+    InvFull = "Nu poti transporta mai multe obiecte"
+}
