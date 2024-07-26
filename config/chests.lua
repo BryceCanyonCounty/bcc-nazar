a03c4192a25962c44f7e011762e63ff62fe2cee5
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-07-25 19:33:47 -0700

    Create chests.lua

---
+++ config/chests.lua
@@ -0,0 +1,193 @@
+Chests = {
+    {
+        huntname = 'Grizzlies West',                  --the name that will show when buying a hint from nazar
+        location = vector3(-960.33, 1630.17, 246.58), -- the location of the chest
+        hintcost = 5,                                 --set to 0 if you dont want to charge the player for getting a hint
+        Reward = { -- you can add as many rewards per chest as you want / use the existing rewards as a guide to add more
+            {
+                displayname = 'Diamond',              --the name of the item that the notification will display
+                name = 'diamond',                     --this has to be the database name of the item
+                count = 1,                            --the amount of the item you want it to give
+            },
+            {
+                displayname = 'Gold Nugget',
+                name = 'gold_nugget',
+                count = 2,
+            },
+            {
+                displayname = 'Iron',
+                name = 'iron',
+                count = 5,
+            },
+        },
+    },
+    -----------------------------------------------------
+    {
+        huntname = 'Girzzlies east',
+        location = vector3(-2728.65, -1464.78, 153.06),
+        hintcost = 5,
+        Reward = {
+            {
+                displayname = 'Book', --the name of the item that the notification will display
+                name = 'book',
+                count = 2,
+            },
+            {
+                displayname = 'Rolling Paper', --the name of the item that the notification will display
+                name = 'rollingpaper',
+                count = 5,
+            },
+            {
+                displayname = 'Beer', --the name of the item that the notification will display
+                name = 'beer',
+                count = 2,
+            },
+        },
+    },
+    -----------------------------------------------------
+    {
+        huntname = 'Big Valley',
+        location = vector3(-2260.32, 115.96, 248.63),
+        hintcost = 5,
+        Reward = {
+            {
+                displayname = 'Stolen Merch', --the name of the item that the notification will display
+                name = 'stolenmerch',
+                count = 2,
+            },
+            {
+                displayname = 'Pipe', --the name of the item that the notification will display
+                name = 'pipe',
+                count = 1,
+            },
+        },
+    },
+    -----------------------------------------------------
+    {
+        huntname = 'Manzanita Post',
+        location = vector3(-1814.82, -1546.38, 95.36),
+        hintcost = 5,
+        Reward = {
+            {
+                displayname = 'Lockpick', --the name of the item that the notification will display
+                name = 'lockpick',
+                count = 1,
+            },
+            {
+                displayname = 'Alligator Tooth', --the name of the item that the notification will display
+                name = 'aligatorto',
+                count = 4,
+            },
+            {
+                displayname = 'Cigar', --the name of the item that the notification will display
+                name = 'cigar',
+                count = 2,
+            },
+        },
+    },
+    -----------------------------------------------------
+    {
+        huntname = 'Lake Don Julio',
+        location = vector3(-3340.63, -3399.11, 31.46),
+        hintcost = 5,
+        Reward = {
+            {
+                displayname = 'Heroin', --the name of the item that the notification will display
+                name = 'heroin',
+                count = 2,
+            },
+            {
+                displayname = 'Book', --the name of the item that the notification will display
+                name = 'book',
+                count = 1,
+            },
+            {
+                displayname = 'Lockpick', --the name of the item that the notification will display
+                name = 'lockpick',
+                count = 1,
+            },
+            {
+                displayname = 'Gold Nugget', --the name of the item that the notification will display
+                name = 'gold_nugget',
+                count = 2,
+            },
+        },
+    },
+    -----------------------------------------------------
+    {
+        huntname = 'Glacier',
+        location = vector3(-1495.65, 3129.55, 492.6),
+        hintcost = 10,
+        Reward = {
+            {
+                displayname = 'Gold Bar', --the name of the item that the notification will display
+                name = 'goldbar',
+                count = 1,
+            },
+            {
+                displayname = 'Lockpick', --the name of the item that the notification will display
+                name = 'lockpick',
+                count = 1,
+            },
+        },
+    },
+    -----------------------------------------------------
+    {
+        huntname = 'Ambarinor',
+        location = vector3(922.24, 2065.61, 310.88),
+        hintcost = 5,
+        Reward = {
+            {
+                displayname = 'Gold Nugget', --the name of the item that the notification will display
+                name = 'gold_nugget',
+                count = 3,
+            },
+            {
+                displayname = 'Stolen Merch', --the name of the item that the notification will display
+                name = 'stolenmerch',
+                count = 1,
+            },
+        },
+    },
+    -----------------------------------------------------
+    {
+        huntname = 'Guarma',
+        location = vector3(1663.53, -7164.55, 93.17),
+        hintcost = 5,
+        Reward = {
+            {
+                displayname = 'Gold Bar', --the name of the item that the notification will display
+                name = 'goldbar',
+                count = 1,
+            },
+            {
+                displayname = 'Stolen Merch', --the name of the item that the notification will display
+                name = 'stolenmerch',
+                count = 1,
+            },
+            {
+                displayname = 'Gold Nugget', --the name of the item that the notification will display
+                name = 'gold_nugget',
+                count = 2,
+            },
+        },
+    },
+    -----------------------------------------------------
+    {
+        huntname = 'Lagras',
+        location = vector3(2299.25, -621.2, 41.25),
+        hintcost = 5,
+        Reward = {
+            {
+                displayname = 'Gold Nugget', --the name of the item that the notification will display
+                name = 'gold_nugget',
+                count = 3,
+            },
+            {
+                displayname = 'Alligator Tooth', --the name of the item that the notification will display
+                name = 'aligatorto',
+                count = 2,
+            },
+        },
+    },
+}
