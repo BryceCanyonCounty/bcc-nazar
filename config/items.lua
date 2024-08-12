ac248ca76a722fd1c78b93fcf5a350cf9ac731b4
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-08-12 16:39:49 -0700

    Create items.lua

---
+++ config/items.lua
@@ -0,0 +1,25 @@
+Items = {}
+
+Items.sell = { -- Items to Sell to Nazar / Add as Many as You Want
+    --itemdbname is the database name of the item and displayname is what will show in the menu with nazar
+    { itemdbname = 'stolenmerch',  displayname = 'Stolen Items',    price = 12, currencytype = 'money' },
+    { itemdbname = 'gold_nugget',  displayname = 'Gold Nugget',     price = 3,  currencytype = 'gold'  },
+    { itemdbname = 'diamond',      displayname = 'Diamond',         price = 10, currencytype = 'money' },
+    { itemdbname = 'goldbar',      displayname = 'Gold Bar',        price = 50, currencytype = 'money' },
+    { itemdbname = 'pipe',         displayname = 'Pipe',            price = 4,  currencytype = 'money' },
+    { itemdbname = 'rollingpaper', displayname = 'Rolling Paper',   price = 2,  currencytype = 'money' },
+    { itemdbname = 'beer',         displayname = 'Old Beer',        price = 5,  currencytype = 'money' },
+    { itemdbname = 'book',         displayname = 'Dusty Book',      price = 3,  currencytype = 'money' },
+    { itemdbname = 'aligatorto',   displayname = 'Alligator Tooth', price = 4,  currencytype = 'money' },
+    { itemdbname = 'heroin',       displayname = 'Heroin',          price = 5,  currencytype = 'money' },
+}
+-----------------------------------------------------
+
+Items.buy = { -- Items to Purchase from Nazar / Add as Many as You Want
+    --itemdbname is the database name of the item and displayname is what will show in the menu with nazar
+    { itemdbname = 'lockpick',                displayname = 'Lock Pick',          price = '5',  currencytype = 'gold' },
+    { itemdbname = 'dynamite',                displayname = 'Dynamite',           price = '20', currencytype = 'gold' },
+    { itemdbname = 'robbingkit',              displayname = 'Robbing Kit',        price = '10', currencytype = 'gold' },
+    { itemdbname = 'consumable_lock_breaker', displayname = 'Advanced Lock Pick', price = '10', currencytype = 'gold' },
+}
+-----------------------------------------------------
